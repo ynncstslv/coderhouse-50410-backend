731b8de4bf34053e238d2f8fc52cfeeb98147539
ynncstslv <ynncstslv@gmail.com>
2023-11-06 13:15:30 -0300

    fix: had to redo addProductToCart logic

--- src/controllers/cartsController.js
+++ src/controllers/cartsController.js
@@ -1,7 +1,11 @@
 import CartManager from '../models/CartManager.js';
+import ProductManager from '../models/ProductManager.js';
 
 const cartFilePath = 'carts.json';
 const cartManager = new CartManager(cartFilePath);
+
+const productFilePath = 'products.json';
+const productManager = new ProductManager(productFilePath);
 
 export function createCartController(req, res) {
 	const newCart = cartManager.createCart();
@@ -24,13 +28,30 @@ export function addProductToCartController(req, res) {
 	const cartId = req.params.cid;
 	const productId = req.params.pid;
 
-	const cart = cartManager.addProductToCart(cartId, productId);
+	const cart = cartManager.getCartById(cartId);
 
 	if (cart) {
-		res.json(cart);
+		const product = productManager
+			.getProducts()
+			.find((product) => product.id === parseInt(productId));
+
+		if (product) {
+			const productIndex = cart.products.findIndex(
+				(item) => item.product === productId
+			);
+
+			if (productIndex !== -1) {
+				cart.products[productIndex].quantity++;
+			} else {
+				cart.products.push({ product: productId, quantity: 1 });
+			}
+
+			cartManager.saveCarts();
+			res.json(cart);
+		} else {
+			res.status(404).json({ error: '[-] Product not found [-]' });
+		}
 	} else {
-		res
-			.status(404)
-			.json({ error: '[-] cart not found or product does not exist [-]' });
+		res.status(404).json({ error: '[-] Cart not found [-]' });
 	}
 }
