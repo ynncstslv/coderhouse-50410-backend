314ed62fffa7c9e19d257a5ccaf2ebb6cc39118e
ynncstslv <ynncstslv@gmail.com>
2023-11-06 12:03:06 -0300

    fix: update index

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import middleware from './src/middlewares/middleware.js';
 import config from './src/config/config.js';
 import productsRoutes from './src/routes/productsRoutes.js';
+import cartsRoutes from './src/routes/cartsRoutes.js';
 
 const app = express();
 const port = config.port;
@@ -10,6 +11,7 @@ const port = config.port;
 middleware(app);
 
 app.use('/products', productsRoutes);
+app.use('/carts', cartsRoutes);
 
 app.listen(port, () => {
 	console.log(`[+] server is running on port ${port} [+]`);
